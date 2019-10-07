d377bbfd6056ae05c28a119f301fc77cf512e614
Im-perfect <zhangke.kerri@gmail.com>
2019-10-07 16:10:43 +0200

    adjusted apirequst location to App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import { Route } from "react-router-dom";
 import Startpage from "./components/Startpage";
 import Doglist from "./components/Doglist";
 import DogDetails from "./components/DogDetails";
+import superagent from "superagent";
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header></header>
-      <Route path="/" exact component={Startpage}></Route>
-      <Route path="/dog-breeds" component={Doglist}></Route>
-      {/* <Route path="/dog-breeds/:breed" component={DogDetails}></Route> */}
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    superagent
+      .get("https://dog.ceo/api/breeds/list/all")
+      .then(res =>
+        this.props.dispatch({
+          type: "INIT_BREEDS",
+          payload: Object.keys(res.body.message)
+        })
+      )
+      .catch(err => console.err(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header></header>
+        <Route path="/" exact component={Startpage}></Route>
+        <Route path="/dog-breeds" component={Doglist}></Route>
+        {/* <Route path="/dog-breeds/:breed" component={DogDetails}></Route> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
