e83190d96a0409918c53ce1b2c5748fc89cb91b8
Im-perfect <zhangke.kerri@gmail.com>
2019-10-08 14:49:07 +0200

    Show result on frontend. Removed submit button

--- src/components/Game1.js
+++ src/components/Game1.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import superagent from "superagent";
+import ResultGame1 from "./ResultGame1";
 
 class Game1 extends React.Component {
   state = {
@@ -9,7 +10,7 @@ class Game1 extends React.Component {
     ],
     imgURL: null,
     answers: [],
-    currentAnswer: null
+    result: null
   };
 
   componentDidMount() {
@@ -22,9 +23,9 @@ class Game1 extends React.Component {
       )
       .catch(err => console.log(err));
 
-      this.setState({
-          answers: this.getAnswers(this.props.currentBreeds)
-      })
+    this.setState({
+      answers: this.getAnswers(this.props.currentBreeds)
+    });
   }
 
   getAnswers = currentBreeds => {
@@ -39,28 +40,44 @@ class Game1 extends React.Component {
   };
 
   checkAnswer = answer => {
-    return answer === this.state.breed ? "WOOF! Good job!" : "BARK! NEE!!";
+    return answer === this.state.breed ? this.setState({
+        result: true
+    }):this.setState({
+        result: false
+    })
   };
 
   render() {
     return (
       <div>
         <h3>Which is the dog breed in the picture?</h3>
-        <img src={this.state.imgURL} alt={this.state.breed} />
-        
+        <img src={this.state.imgURL} alt={this.state.breed} height='250px' />
+
         <h4>Choose from:</h4>
         <div>
-          <button id="A" onClick={()=>this.setState({ currentAnswer: this.state.answers[0] })}>
+          <button
+            id="A"
+            // onClick={() =>
+            //   this.setState({ currentAnswer: this.state.answers[0] })
+            // }
+            onClick={()=>this.checkAnswer(this.state.answers[0])}
+          >
             {this.state.answers[0]}
           </button>
-          <button id="B" onClick={()=>this.setState({ currentAnswer: this.state.answers[1] })}>
+          <button
+            id="B"
+            onClick={()=>this.checkAnswer(this.state.answers[1])}
+          >
             {this.state.answers[1]}
           </button>
-          <button id="C" onClick={()=>this.setState({ currentAnswer: this.state.answers[2] })}>
+          <button
+            id="C"
+            onClick={()=>this.checkAnswer(this.state.answers[2])}
+          >
             {this.state.answers[2]}
           </button>
         </div>
-        <button onClick={()=>this.checkAnswer(this.state.currentAnswer)}>Submit</button>
+        <ResultGame1 result={this.state.result}/>
       </div>
     );
   }
