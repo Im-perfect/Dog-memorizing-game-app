54d3bbfab3b76afd2a1c126f7645f1ebb6d5323a
Im-perfect <zhangke.kerri@gmail.com>
2019-10-11 00:48:30 +0200

    style list

--- src/components/Doglist.js
+++ src/components/Doglist.js
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 class Doglist extends React.Component {
   render() {
     return <div>
-        <ul>
+        <ul id="list">
             {this.props.dogbreeds.map(breed => {
                 return <li key={breed}><Link to={`/dog-breeds/${breed}`}>{breed}</Link></li>
             })}
