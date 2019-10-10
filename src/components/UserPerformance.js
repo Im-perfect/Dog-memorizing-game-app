ed6d4d8acfcf6a689bfecca6323a119570a673d5
Im-perfect <zhangke.kerri@gmail.com>
2019-10-10 23:59:45 +0200

    style favicon, perfermance bar

--- src/components/UserPerformance.js
+++ src/components/UserPerformance.js
@@ -1,28 +1,35 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import Header from './Header'
+import React from "react";
+import { connect } from "react-redux";
+import Header from "./Header";
+import "../css/main.css"
 
 class UserPerformance extends React.Component {
-    render() {
-        const{rightAnswers,totalAnswers,level} = this.props.answers
-        return(
-            <div className = 'UserPerformance'>
-                 <Header/>
-                <ul>
-                <li>RightAnswers: {rightAnswers}</li>
-                <li>TotalAnswers: {totalAnswers}</li>
-                <li>SuccessRate: {totalAnswers === 0 ? 0 :(rightAnswers/totalAnswers*100).toFixed(0)}%</li>
-                <li>Level: {level}</li>
-                <li>Current Breeds:{this.props.currentBreeds.length}</li>
-                </ul>
-            </div>
-        )
-    }
-}
-const mapStateToProps = (state) => {
-    return {
-      answers: state.answers,
-      currentBreeds: state.currentBreeds
-    }
+  render() {
+    const { rightAnswers, totalAnswers, level } = this.props.answers;
+    return (
+      <div className="UserPerformance">
+        <Header />
+        <ul id='performance'>
+          <li>RightAnswers: {rightAnswers}</li>
+          <li>TotalAnswers: {totalAnswers}</li>
+          <li>
+            SuccessRate:{" "}
+            {totalAnswers === 0
+              ? 0
+              : ((rightAnswers / totalAnswers) * 100).toFixed(0)}
+            %
+          </li>
+          <li>Level: {level}</li>
+          <li>Current Breeds:{this.props.currentBreeds.length}</li>
+        </ul>
+      </div>
+    );
   }
-export default connect(mapStateToProps)(UserPerformance)
+}
+const mapStateToProps = state => {
+  return {
+    answers: state.answers,
+    currentBreeds: state.currentBreeds
+  };
+};
+export default connect(mapStateToProps)(UserPerformance);
